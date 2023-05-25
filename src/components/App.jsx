2c69a4985dffde307fc11d7546dbf13c9d39069f
Dezertirr <125862316+Dezertirr@users.noreply.github.com>
2023-05-25 18:25:07 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import shortid from "shortid";
+
+
+
+export class App extends Component {
+  state = {
+  name: '' ,
+  number: '' ,
+  contacts: []
 };
+
+handleChange = evt => {
+const {name, value} = evt.currentTarget
+  this.setState ({
+    [name]: value
+  })
+};
+
+handleSubmit = evt => {
+  evt.preventDefault();
+  const {name, number} = this.state
+  const newContact = {
+    id: shortid.generate,
+    name: name,
+    number: number,
+  }
+
+  this.setState (prevState => ({
+    contacts: [...prevState.contacts, newContact],
+    name:'',
+    number: '',
+  }))
+
+};
+
+
+render() {
+
+
+  return (
+    <div>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor=""> Name
+        <input type="text"
+        name='name'
+        value={this.state.name}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        onChange={this.handleChange}
+         />
+        </label>
+
+        <label htmlFor=""> Number
+        <input type="text"
+        name="number"
+        value={this.state.number}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Create" />
+
+      </form>
+   <ul>
+    {this.state.contacts.map(contact => (
+      <li key={contact.id}>
+        <h4>{contact.name}</h4>
+        <p>{contact.number}</p>
+      </li>
+    ))}
+
+   </ul>
+    </div>
+  )
+}
+
+}
